cdedda0e0463d6bbc41a8daad6a2e9fcbb4cdb0f
Tim <timg51237@gmail.com>
2018-06-20 18:30:44 -0700

    Node.js

---
+++ etherball-frontend/pages/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const dummy = props => {
+    return (
+        <div>
+            Hello World
+        </div>
+    )
+}
+
+export default dummy;
--- etherball-frontend/src/hoc/aaux.js
+++
@@ -1,5 +0,0 @@
-//Higher order component to wrap multiple components without changing styling
-
-const aaux = (props) => props.children;
-
-export default aaux;
