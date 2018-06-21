6a42f8fb732cd0e7f6e0bd2b5cb30033ad12009a
rsimpsonn <ryan@bonfr.com>
2018-06-20 19:01:42 -0700

    Basic error page.

---
+++ etherball-frontend/pages/_error.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import styled from "styled-components";
+
+const ErrorPage = props => {
+  return (
+    <div>
+      <p>You have reached an error!</p>
+    </div>
+  );
+};
+
+export default ErrorPage;
